0c4a571412c1592c0f8a9871b97b0882990faf25
chakunyoung <chakunyou@naver.com>
2021-10-29 15:07:04 +0900

    database 서버 연동

--- config/settings.py
+++ config/settings.py
@@ -56,8 +56,11 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'OPTIONS': {
+            'sql_mode': 'traditional',
+            'read_default_file': os.path.join(BASE_DIR, 'mysql.cnf')
+        }
     }
 }
 
