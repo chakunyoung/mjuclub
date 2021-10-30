79a23312f32b02bc40e824659203ed551b90080b
chakunyoung <chakunyou@naver.com>
2021-10-30 15:28:39 +0900

    로그인 css 수정

--- config/settings.py
+++ config/settings.py
@@ -101,7 +101,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'config', 'static'),  ################### BASE_DIR/config/static
+    os.path.join(BASE_DIR, 'static'),  ################### BASE_DIR/config/static
 )
 
 
