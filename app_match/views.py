e71d7891c27aaa74e9f0fcd95ca9538f1df65279
chakunyoung <chakunyou@naver.com>
2021-11-22 01:50:06 +0900

    club_info, match ui 조정

--- app_match/views.py
+++ app_match/views.py
@@ -8,11 +8,11 @@ def match(req):
 def match_result(req):
     feature_list =[]
     if req.method == 'POST':
-        feature0 = req.POST.get('feature0')
-        feature1 = req.POST.get('feature1')
-        feature2 = req.POST.get('feature2')
-        feature3 = req.POST.get('feature3')
-        feature4 = req.POST.get('feature4')
+        feature0 = req.POST.get('feature0')  # 개방
+        feature1 = req.POST.get('feature1')  # 친화
+        feature2 = req.POST.get('feature2')  # 정서
+        feature3 = req.POST.get('feature3')  # 성실
+        feature4 = req.POST.get('feature4')  # 외향
 
         print(feature0)
         feature_list = [feature0, feature1, feature2, feature3, feature4]
